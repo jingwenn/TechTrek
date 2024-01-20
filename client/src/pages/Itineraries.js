aa313bde223523e4b27e24e7476d94842e6d915e
jingwenn <jingwenntang@gmail.com>
2024-01-20 11:03:12 +0800

    move to pages

---
+++ client/src/pages/Itineraries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Itineraries = () => {
+  return (
+    <div>
+      <h3>Itinerary Title</h3>
+      <p>Country: {{}}</p>
+    </div>
+  )
+}
+
+export default Itineraries
