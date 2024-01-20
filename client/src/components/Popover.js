c8c80df4888c3af90101fadbffcef63ca6182b4b
Saravanamani Ramalingam <ramalingamsaravanamani1999@gmail.com>
2024-01-20 12:08:45 +0800

    Finish adding popover

--- client/src/components/Popover.js
+++ client/src/components/Popover.js
@@ -1,6 +1,6 @@
 import { React, Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
-import {Dialog, Transition} from '@headlessui/react';
+import {Dialog, Transition, Switch} from '@headlessui/react';
 import { useForm } from 'react-hook-form'
 
 
@@ -12,6 +12,8 @@ const Popover = (props) => {
   // const closePopover = () => {
   //   setIsOpen(false);
   // }
+
+  const [isSelect, setIsSelect] = useState(true);
 
   const {register, handleSubmit, formState: errors} = useForm()
 
@@ -51,22 +53,49 @@ const Popover = (props) => {
                   </Dialog.Title>
                   <div className="mt-2">
                     <form onSubmit={handleSubmit(props.closeModal)}>
-                      {/* <label type="text" className='text-black flex flex-col w-full'>
+                    <div className="py-2">
+                  </div>
+                      <label type="text" className='text-black flex flex-col w-full'>
+                          <span className='w-full flex justify-between'>
                             Name
-                          <input className='border my-2' {...register('name')}/>
+                              <Switch
+                                checked={isSelect}
+                                onChange={setIsSelect}
+                                className={`${isSelect ? 'bg-teal-900' : 'bg-teal-700'}
+                                  relative inline-flex w-1/12 h-[1rem] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white/75 mr-5`}
+                              >
+                                <span
+                                  aria-hidden="true"
+                                  className={`${isSelect ? 'translate-x-4' : 'translate-x-0'}
+                                    pointer-events-none inline-block h-[calc(100%)] w-1/2 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                                />
+                              </Switch>
+                            </span>
+                          {
+                            isSelect ? (
+                              <select className='border py-0.5 my-2 rounded' {...register('name')}>
+                                <option>
+                                  name
+                                </option>
+                              </select>
+                            ) : (
+                              <input type="text" className='border my-2 rounded' {...register('name')}/>
+                            )
+                          }
+                          
                       </label>
                       <label className='text-black flex flex-col w-full'>
                             Cost
-                          <input type="number" className='border my-2' {...register('cost')}/>
+                          <input type="number" className='border my-2 rounded' {...register('cost')}/>
                       </label>
                       <label className='text-black flex flex-col w-full'>
                             Notes
-                          <input type="text" className='border my-2' {...register('notes')}/>
-                      </label> */}
+                          <input type="text" className='border my-2 rounded' {...register('notes')}/>
+                      </label>
                       <div className="mt-4 flex justify-end">
                           <button
                             type="button"
-                            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                            className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
                             onClick={props.closeModal}
                           >
                             Save
@@ -83,77 +112,6 @@ const Popover = (props) => {
         </Dialog>
       </Transition>
     </>
-    // <>
-    //   <Transition appear show={props.isOpen} as={Fragment}>
-    //     <Dialog as={div} className="relative z-10" onClose={props.closeModal}>
-    //       <Transition.Child
-    //         as={Fragment}
-    //         enter="ease-out duration-300"
-    //         enterFrom="opacity-0"
-    //         enterTo="opacity-100"
-    //         leave="ease-in duration-200"
-    //         leaveFrom="opacity-100"
-    //         leaveTo="opacity-0"
-    //       >
-    //         <div className="fixed inset-0 bg-black/25"/>
-    //       </Transition.Child>
-    //       <div className="">
-    //         <div>
-    //           <Transition.Child
-    //             as={Fragment}
-    //             enter="ease-out duration-300"
-    //             enterFrom="opacity-0 scale-95"
-    //             enterTo="opacity-100 scale-100"
-    //             leave="ease-in duration-200"
-    //             leaveFrom="opacity-100 scale-100"
-    //             leaveTo="opacity-0 scale-95"
-    //           >
-    //             <Dialog.panel className="w-full max-w-md w-full transform ">
-    //               <Dialog.Title 
-    //                 as={h3}
-    //                 className="text-lg font-medium text-gray-900 leading-6"
-    //               >
-    //                 Destination Information
-    //               </Dialog.Title>
-    //               <div className="text-sm text-gray-500">
-    //                 <form>
-    //                   <label>
-    //                     Destination name
-    //                     <input 
-    //                       type="text"
-
-    //                     />
-    //                   </label>
-    //                   <label>
-    //                     Cost
-    //                     <input
-    //                       type="number" 
-    //                     />
-    //                   </label>
-    //                   <label>
-    //                     Notes
-    //                     <input 
-    //                       type="text"
-    //                     />
-    //                   </label>
-    //                   <div className="mt-4 flex w-full justify-end">
-    //                     <button>
-    //                       Save
-    //                     </button>
-    //                   </div>
-                      
-    //                 </form>
-
-    //               </div>
-
-    //             </Dialog.panel>
-    //           </Transition.Child>
-    //         </div>
-    //       </div>
-
-    //     </Dialog>
-    //   </Transition>      
-    // </>
   );
 }
 
