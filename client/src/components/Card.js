87d885344c7888dcc302a7f7b7a78a5b42e1dd7e
Saravanamani Ramalingam <ramalingamsaravanamani1999@gmail.com>
2024-01-20 12:30:38 +0800

    Add card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = (props) => {
+  return (
+    <div className="w-3/4 h-[8rem] bg-white shadow-lg rounded-lg flex justify-between py-8">
+        <div className="flex flex-col items-center justify-between h-full md:pl-10">
+            <p>{props.data.name}</p>
+            <p>$ {props.data.cost}</p>
+        </div>
+        <div className="flex flex-col items-start pr-3">
+            <span onClick={props.edit}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mb-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+            </span>
+            <span onClick={props.remove}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+Card.propTypes = {
+
+}
+
+export default Card
