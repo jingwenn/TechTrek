894d27b68950e54b492b6dc1870c42cf6f124f40
Saravanamani Ramalingam <ramalingamsaravanamani1999@gmail.com>
2024-01-20 13:12:15 +0800

    Add get all itenararies call

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import {React, useState} from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 
 const Home = props => {
+  const [allItenararies, setAllItenararies] = useState({});
+
+  const getItenararies = async (req, res) => {
+    axios.get('http://localhost:4000/itenarary/',{
+      headers: {
+        'Content-Type' : 'application/json',
+        'Cookie' : 'uid'
+      }
+    }).then(() => {
+      setAllItenararies(res.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
     <div>
 
