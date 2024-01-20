3a09fa1ec41fef6f4937d501de3aeba11169e916
alift99 <alifdaffa.main@gmail.com>
2024-01-20 12:32:59 +0800

    Improved login interface

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import { useState, useContext } from 'react';
+import axios from 'axios';
 
 const Login = props => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
     const handleLogin = (data) => {
-        // To-Do
-        console.log('Attempted to Login with ' + data);
+        
     }
 
     return (
-        <div className='flex flex-col w-1/4 border border-black rounded-md p-9 justify-start'>
-            <form>
-                <h2>Login Page</h2>
-                <div>Username:
-                    <input></input>
-                </div>
-                <div>Password: 
-                    <input type='password'></input>
-                </div>
-                <button onClick={handleLogin}>Login</button>
-            </form>
+        <div className="flex min-h-screen flex-col items-center p-24 bg-white">
+            <div className='flex flex-col w-1/4 border border-black rounded-md p-9 justify-start'>
+                <form>
+                    <span style={{display:"block", fontWeight: "bold", marginBottom:6, textAlign: "center"}}>Login Page</span>
+                    <div>Username:
+                        <input 
+                            className='border w-full' 
+                            type="email"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />                
+                    </div>
+                    <div>Password: 
+                        <input 
+                            className='border w-full' 
+                            type='password' 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className='flex justify-center items-center'>
+                        <button 
+                            className='text-black border w-1/2 rounded-md my-2 bg-green-300' 
+                            onClick={handleLogin}
+                        >Login
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
